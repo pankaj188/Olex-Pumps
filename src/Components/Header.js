23bc9cc5c5beef2b8c190f9b77e90f4961620998
pankaj188 <118pankaj.kumar@gmail.com>
2023-12-02 19:24:43 +0530

    code changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
 return(
         <div className=" flex text-center bg-gray-50 w-72 md:w-full  sm:m-2 ">
                 <div className="logo-container ">
-                <img className="w-52 max-w-sm max-w-md h-48" alt= "logo" src={logo}/>
+                <img className="w-52  max-w-md h-48" alt= "logo" src={logo}/>
                 </div>
                 <Link to="/products"><div className="flex items-center md:place-items-center w-[300px] mt-20 m-2 text-2xl sm:text-3xl  font-bold ">Olex Pumps & Electricals LLP</div></Link>
                 <div className=" flex invisible md:visible md:ml-56 xl:ml-[400px]  justify-start lg:justify-end ">
