3ada7a275e4374374febabe69282d550a6dd460a
pankaj188 <118pankaj.kumar@gmail.com>
2023-12-06 17:51:30 +0530

    code changes

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,10 +7,10 @@ const Navbar = () => {
                     <div className="flex space-x-4 md:place-items-center ">
                             <ul className="flex m-4  text-white  p-2  text-xl lg:text-3xl  ">
                             <Link to="/"><li className="px-4 md:px-40 hover:text-gray-400 ">Home</li></Link>
-                            <li class="dropdown hover:text-gray-400 "><a class="dropdown-toggle" data-toggle="dropdown" >Products <span class="caret"></span></a>
+                            <li class="dropdown hover:text-gray-200"><a class="dropdown-toggle" data-toggle="dropdown" >Products <span class="caret"></span></a>
                             <ul class="dropdown-menu">
-                            <Link  to="/agriculture"><li className="text-black ">Agriculture Pumps</li></Link>
-                            <Link  to="/residential"><li className="text-black">Residential Pumps</li></Link>
+                            <Link  to="/agriculture"><li className="text-gray-500 px-3">Agriculture Pumps</li></Link>
+                            <Link  to="/residential"><li className="text-gray-500 px-3">Residential Pumps</li></Link>
                              </ul>
                             </li>
                             <Link  to="/about"><li className="px-4 md:px-40 hover:text-gray-400 ">About Us</li></Link>
